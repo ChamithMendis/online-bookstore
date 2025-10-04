011879904451a6072180a921cfd9fd61ec62a790
Chamith Mendis <m.chamith@directfn.com>
2025-10-05 00:44:08 +0530

    Deployement related changes

--- frontend/src/app/shared/environments/environment.ts
+++ frontend/src/app/shared/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost:8080', // [TODO:] 'http://52.206.37.75:8080', get environmet file out from source folders
   production: false,
 };
